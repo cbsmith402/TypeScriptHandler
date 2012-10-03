7c266b71f264eeb5bd28e3d82b9497fd717ec612
Chris Smith <cbsmith@cbsmith.org>
2012-10-03 13:57:31 -0400

    First commit

---
+++ TypeScriptHandler/Handler.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.IO;
+using System.Diagnostics;
+
+namespace TypeScriptHandler
+{
+	public class Handler : IHttpHandler
+	{
+		#region IHttpHandler Members
+
+		public bool IsReusable
+		{
+			get { return true; }
+		}
+
+		public void ProcessRequest(HttpContext context)
+		{
+			string tsPath = context.Request.PhysicalPath;
+
+			if (!File.Exists(tsPath))
+			{
+				context.Response.StatusCode = 404;
+				context.Response.End();
+				return;
+			}
+
+			string jsPath = context.Request.PhysicalPath.Replace(".ts", ".js");
+			string tsDirectory = new FileInfo(context.Request.PhysicalPath).Directory.FullName;
+
+			//Check if the file doesn't exist or has been changed since we compiled it
+			if (!File.Exists(jsPath) || File.GetLastWriteTimeUtc(jsPath) < File.GetLastWriteTimeUtc(tsPath))
+			{
+				//Generate or regenerate the JS file
+				//Whee, escaping!
+				ExecuteCommandSync("\"tsc \"" + tsPath + "\"\"", tsDirectory);
+			}
+
+			//Write out the resulting file (we assume it exists if there were no errors written to stderr below)
+			context.Response.WriteFile(jsPath);
+		}
+
+		public string ExecuteCommandSync(string command, string workingDirectory)
+		{
+			// create the ProcessStartInfo using "cmd" as the program to be run,
+			// and "/c " as the parameters.
+			// Incidentally, /c tells cmd that we want it to execute the command that follows,
+			// and then exit.
+			ProcessStartInfo procStartInfo = new ProcessStartInfo("cmd", "/s /c " + command);
+
+			// The following commands are needed to redirect the standard output.
+			// This means that it will be redirected to the Process.StandardOutput StreamReader.
+			procStartInfo.RedirectStandardOutput = true;
+			procStartInfo.RedirectStandardError = true;
+			procStartInfo.UseShellExecute = false;
+			// Do not create the black window.
+			procStartInfo.CreateNoWindow = true;
+			//Set the working directory
+			procStartInfo.WorkingDirectory = workingDirectory;
+
+			// Now we create a process, assign its ProcessStartInfo and start it
+			Process proc = new Process();
+			proc.StartInfo = procStartInfo;
+			proc.Start();
+			proc.WaitForExit();
+
+			var output = proc.StandardOutput.ReadToEnd();
+			var errors = proc.StandardError.ReadToEnd();
+
+			if (!string.IsNullOrWhiteSpace(errors))
+			{
+				throw new Exception(errors);
+			}
+
+			return output;
+		}
+
+		#endregion
+	}
+}
---
+++ TypeScriptHandler/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TypeScriptHandler")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TypeScriptHandler")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("adb1f965-20d5-4569-a2ff-b875be2f09e2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
